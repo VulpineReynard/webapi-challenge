e96ec725bca204a97e6f13996a069849a6d7ecb8
VulpineReynard <milare8@gmail.com>
2019-09-06 11:35:36 -0400

    server is up and running, getting response on sanity check

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+// code away!
+require('dotenv').config();
+const server = require('./server.js');
+const defaults = require('./config/defaults');
+
+server.listen(
+  defaults.port, 
+  () => console.log(`API running on port ${defaults.port}`)
+);
